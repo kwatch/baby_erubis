07d435d24cb7cc998f94a66f3d5db1296a52d400
makoto kuwata <kwa@kuwata-lab.com>
2014-05-03 15:44:21 +0900

    ruby: add 'test/BabyErubis_test.rb'

---
+++ test/BabyErubis_test.rb
@@ -0,0 +1,479 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: 0.0.0 $
+### $Copyright: copyright(c) 2014 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
+libpath = File.join(File.dirname(File.dirname(__FILE__)), 'lib')
+$: << libpath unless $:.include?(libpath)
+
+require 'minitest/autorun'
+
+require 'baby_erubis'
+
+
+
+describe BabyErubis::Template do
+
+  let(:template) { BabyErubis::Text.new() }
+
+
+  describe '#convert()' do
+
+    it "[!118pw] converts template string into ruby code." do
+      input = <<'END'
+<% for x in (1..3) %>
+  i=<%= i %>
+<% end %>
+END
+      expected = <<'END'
+_buf = ''; for x in (1..3) ;
+ _buf << '  i='; _buf << ( i ).to_s; _buf << '
+'; end ;
+; _buf.to_s
+END
+      code = template.convert(input)
+      assert_equal expected, code
+    end
+
+    it "[!7ht59] escapes single quotation and backslash characters." do
+      input = <<'END'
+who's who?
+'''
+\w
+\\\
+END
+      expected = <<'END'
+_buf = ''; _buf << 'who\'s who?
+\'\'\'
+\\w
+\\\\\\
+';; _buf.to_s
+END
+      assert_equal expected, template.convert(input)
+    end
+
+    it "[!u93y5] appends embedded expression in '<%= %>'." do
+      input = <<'END'
+x = <%= x %>
+END
+      expected = <<'END'
+_buf = ''; _buf << 'x = '; _buf << ( x ).to_s; _buf << '
+';; _buf.to_s
+END
+      assert_equal expected, template.convert(input)
+    end
+
+    it "[!auj95] appends embedded expression in '<%= %>' without escaping." do
+      input = <<'END'
+x = <%== x %>
+END
+      expected = <<'END'
+_buf = ''; _buf << 'x = '; _buf << ( x ).to_s; _buf << '
+';; _buf.to_s
+END
+      assert_equal expected, template.convert(input)
+    end
+
+    it "[!qveql] appends linefeeds when '<%# %>' found." do
+      input = <<'END'
+<%#
+  for x in xs
+%>
+x = <%#=
+  x
+ %>
+<%#
+  end
+%>
+END
+      expected = <<'END'
+_buf = '';
+
+ _buf << '
+'; _buf << 'x = ';
+
+ _buf << '
+';
+
+ _buf << '
+';; _buf.to_s
+END
+      assert_equal expected, template.convert(input)
+    end
+
+    it "[!b10ns] generates ruby code correctly even when no embedded code." do
+      input = <<'END'
+abc
+def
+END
+      expected = <<'END'
+_buf = ''; _buf << 'abc
+def
+';; _buf.to_s
+END
+      assert_equal expected, template.convert(input)
+    end
+
+    it "[!3bx3d] not print extra linefeeds when line starts with '<%' and ends with '%>'" do
+      input = <<'END'
+  <% for item in items %>
+    <% if item %>
+    item = <%= item %>
+    <% end %>
+  <% end %>
+END
+      expected = <<'END'
+_buf = '';   for item in items ;
+     if item ;
+ _buf << '    item = '; _buf << ( item ).to_s; _buf << '
+';     end ;
+   end ;
+; _buf.to_s
+END
+      assert_equal expected, template.convert(input)
+    end
+
+  end
+
+
+  describe '#render()' do
+
+    it "renders template with context values." do
+      input = <<'END'
+title: <%= @title %>
+items:
+<% for item in @items %>
+  - <%= item %>
+<% end %>
+END
+      expected = <<'END'
+title: Example
+items:
+  - <AAA>
+  - B&B
+  - "CCC"
+END
+      context = {:title=>'Example', :items=>['<AAA>', 'B&B', '"CCC"']}
+      output = template.compile(input).render(context)
+      assert_equal expected, output
+    end
+
+    it "uses arg as context object when arg is not a hash object." do
+      input = <<'END'
+title: <%= @title %>
+items:
+<% for item in @items %>
+  - <%= item %>
+<% end %>
+END
+      expected = <<'END'
+title: Example
+items:
+  - <AAA>
+  - B&B
+  - "CCC"
+END
+      obj = Object.new
+      obj.instance_variable_set('@title', 'Example')
+      obj.instance_variable_set('@items', ['<AAA>', 'B&B', '"CCC"'])
+      output = template.compile(input).render(obj)
+      assert_equal expected, output
+    end
+
+  end
+
+
+  describe '#compile()' do
+
+    it "compiles template string into proc object." do
+      assert_nil template.instance_variable_get('@_proc')
+      template.compile("x=<%= x %>")
+      assert_kind_of Proc, template.instance_variable_get('@_proc')
+    end
+
+    it "returns self." do
+      assert_same template, template.compile("x=<%= x %>")
+    end
+
+    it "takes filename and linenum." do
+      begin
+        template.compile("x=<%= do end %>", 'example.erb', 3)
+      rescue SyntaxError => ex
+        assert_match /\Aexample\.erb:3: syntax error/, ex.message
+      end
+    end
+
+    it "uses '(eRuby)' as default filename and 1 as default linenum." do
+      begin
+        template.compile("x=<%= do end %>")
+      rescue SyntaxError => ex
+        assert_match /\A\(eRuby\):1: syntax error/, ex.message
+      end
+    end
+
+  end
+
+
+  describe '.load()' do
+
+    it "reads template file and returns template object." do
+      input = <<'END'
+title: <%= @title %>
+items:
+<% for item in @items %>
+  - <%= item %>
+<% end %>
+END
+      expected = <<'END'
+title: Example
+items:
+  - <AAA>
+  - B&B
+  - "CCC"
+END
+      tmpfile = "test.#{rand()}.erb"
+      File.open(tmpfile, 'wb') {|f| f.write(input) }
+      begin
+        template = BabyErubis::TextTemplate.load(tmpfile)
+        context = {:title=>'Example', :items=>['<AAA>', 'B&B', '"CCC"']}
+        output = template.render(context)
+        assert_equal expected, output
+      ensure
+        File.unlink(tmpfile) if File.exist?(tmpfile)
+      end
+    end
+
+    it "reads template file with specified encoding." do
+      input    = "タイトル: <%= @title %>"
+      expected = "タイトル: サンプル"
+      tmpfile = "test.#{rand()}.erb"
+      File.open(tmpfile, 'wb:utf-8') {|f| f.write(input) }
+      begin
+        # nothing should be raised
+        template = BabyErubis::TextTemplate.load(tmpfile, 'utf-8')
+        output = template.render(:title=>"サンプル")
+        assert_equal expected, output
+        assert_equal 'UTF-8', output.encoding.name
+        # exception should be raised
+        ex = assert_raises ArgumentError do
+          template = BabyErubis::TextTemplate.load(tmpfile, 'us-ascii')
+        end
+        assert_equal "invalid byte sequence in US-ASCII", ex.message
+      ensure
+        File.unlink(tmpfile) if File.exist?(tmpfile)
+      end
+    end
+
+  end
+
+
+end
+
+
+
+describe BabyErubis::TextTemplate do
+
+  input = <<'END'
+title: <%= @title %>
+items:
+<% for item in @items %>
+  - <%= item %>
+<% end %>
+END
+  source = <<'END'
+_buf = ''; _buf << 'title: '; _buf << ( @title ).to_s; _buf << '
+'; _buf << 'items:
+'; for item in @items ;
+ _buf << '  - '; _buf << ( item ).to_s; _buf << '
+'; end ;
+; _buf.to_s
+END
+  output = <<'END'
+title: Example
+items:
+  - <AAA>
+  - B&B
+  - "CCC"
+END
+
+
+  describe '#convert()' do
+
+    it "handles embedded expression with no escaping." do
+      tmpl = BabyErubis::Text.new(input)
+      assert_equal source, tmpl.src
+    end
+
+  end
+
+
+  describe '#render()' do
+
+    it "renders context values with no escaping." do
+      tmpl = BabyErubis::Text.new(input)
+      context = {:title=>'Example', :items=>['<AAA>', 'B&B', '"CCC"']}
+      assert_equal output, tmpl.render(context)
+    end
+
+  end
+
+
+end
+
+
+
+describe BabyErubis::HtmlTemplate do
+
+  input = <<'END'
+<html>
+  <h1><%= @title %></h1>
+  <ul>
+    <% for item in @items %>
+    <!-- <%== item %> -->
+    <li><%= item %></li>
+    <% end %>
+  </ul>
+</html>
+END
+  source = <<'END'
+_buf = ''; _buf << '<html>
+  <h1>'; _buf << escape( @title ); _buf << '</h1>
+  <ul>
+';     for item in @items ;
+ _buf << '    <!-- '; _buf << ( item ).to_s; _buf << ' -->
+    <li>'; _buf << escape( item ); _buf << '</li>
+';     end ;
+ _buf << '  </ul>
+</html>
+';; _buf.to_s
+END
+  output = <<'END'
+<html>
+  <h1>Example</h1>
+  <ul>
+    <!-- <AAA> -->
+    <li>&lt;AAA&gt;</li>
+    <!-- B&B -->
+    <li>B&amp;B</li>
+    <!-- "CCC" -->
+    <li>&quot;CCC&quot;</li>
+  </ul>
+</html>
+END
+
+
+  describe '#convert()' do
+
+    it "handles embedded expression with escaping." do
+      tmpl = BabyErubis::Html.new(input)
+      assert_equal source, tmpl.src
+    end
+
+  end
+
+
+  describe '#render()' do
+
+    it "renders context values with escaping." do
+      tmpl = BabyErubis::Html.new(input)
+      context = {:title=>'Example', :items=>['<AAA>', 'B&B', '"CCC"']}
+      assert_equal output, tmpl.render(context)
+    end
+
+  end
+
+
+end
+
+
+
+describe 'BabyErubis::TemplateContext' do
+
+
+  describe '#initialize()' do
+
+    it "[!p69q1] takes hash object and sets them into instance variables." do
+      ctx = BabyErubis::TemplateContext.new(:x=>10, :y=>20)
+      assert_equal 10, ctx.instance_variable_get('@x')
+      assert_equal 20, ctx.instance_variable_get('@y')
+      assert_equal [:@x, :@y], ctx.instance_variables
+    end
+
+    it "[!p853f] do nothing when vars is nil." do
+      ctx = BabyErubis::TemplateContext.new(nil)
+      assert_equal [], ctx.instance_variables
+    end
+
+  end
+
+
+  describe '#[]' do
+
+    it "returns context value." do
+      ctx = BabyErubis::TemplateContext.new(:x=>10)
+      assert_equal 10, ctx[:x]
+    end
+
+  end
+
+
+  describe '#[]=' do
+
+    it "returns context value." do
+      ctx = BabyErubis::TemplateContext.new
+      ctx[:y] = 20
+      assert_equal 20, ctx[:y]
+    end
+
+  end
+
+
+end
+
+
+
+describe 'BabyErubis::TextTemplateContext' do
+
+  let(:ctx) { BabyErubis::TextTemplateContext.new }
+
+
+  describe '#escape()' do
+
+    it "converts any value into string." do
+      assert_equal '10',   ctx.escape(10)
+      assert_equal 'true', ctx.escape(true)
+      assert_equal '',     ctx.escape(nil)
+      assert_equal '["A", "B"]', ctx.escape(['A', 'B'])
+    end
+
+    it "does not escape html special chars." do
+      assert_equal '<>&"',   ctx.escape('<>&"')
+    end
+
+  end
+
+
+end
+
+
+
+describe 'BabyErubis::HtmlTemplateContext' do
+
+  let(:ctx) { BabyErubis::HtmlTemplateContext.new }
+
+
+  describe '#escape()' do
+
+    it "escapes html special chars." do
+      assert_equal '&lt;&gt;&amp;&quot;&#39;', ctx.escape('<>&"\'')
+      assert_equal '&lt;a href=&quot;?x=1&amp;y=2&amp;z=3&quot;&gt;click&lt;/a&gt;',
+                   ctx.escape('<a href="?x=1&y=2&z=3">click</a>')
+    end
+
+  end
+
+
+end
