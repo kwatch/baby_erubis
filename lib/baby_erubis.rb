70d72fa374694dabae611336aee4a2cad1061d55
makoto kuwata <kwa@kuwata-lab.com>
2014-05-05 22:40:01 +0900

    ruby: change 'Template#from_file()' and '#from_str()' to follow new '#compile()'

--- lib/baby_erubis.rb
+++ lib/baby_erubis.rb
@@ -29,12 +29,12 @@ module BabyErubis
 
     def from_file(filename, encoding='utf-8')
       input = File.open(filename, "rb:#{encoding}") {|f| f.read() }
-      compile(input, filename, 1)
+      compile(convert(input), filename, 1)
       return self
     end
 
     def from_str(input, filename=nil, linenum=1)
-      compile(input, filename, linenum)
+      compile(convert(input), filename, linenum)
       return self
     end
 
