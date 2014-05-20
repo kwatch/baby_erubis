d46a9719f6e2c4a9ef4ea100d10c71c0005167ef
makoto kuwata <kwa@kuwata-lab.com>
2014-05-20 22:42:55 +0900

    ruby: define 'RailsTemplate' class

---
+++ lib/baby_erubis/rails.rb
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: $
+### $Copyright: copyright(c) 2014 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
+require 'baby_erubis'
+
+
+module BabyErubis
+
+
+  class RailsTemplate < Template
+
+    protected
+
+    def add_preamble(src)
+      src << "@output_buffer = output_buffer || ActionView::OutputBuffer.new;"
+    end
+
+    def add_postamble(src)
+      src << "@output_buffer.to_s\n"
+    end
+
+    def add_text(src, text)
+      return if !text || text.empty?
+      freeze = @freeze ? '.freeze' : ''
+      text.gsub!(/['\\]/, '\\\\\&')
+      src << "@output_buffer.safe_append='#{text}'#{freeze};"
+    end
+
+    def add_stmt(src, stmt)
+      return if !stmt || stmt.empty?
+      src << stmt
+    end
+
+    def add_expr(src, expr, indicator)
+      return if !expr || expr.empty?
+      has_block = expr =~ /(\bdo|\{) *(\|.*?\| *)?\z/
+      l = '('; r = ')'
+      l = r = ' ' if has_block
+      if indicator == '='           # escaping
+        src << "@output_buffer.append=#{l}#{expr}#{r};"
+      else                          # without escaping
+        src << "@output_buffer.safe_append=#{l}#{expr}#{r};"
+      end
+    end
+
+  end
+
+
+end
---
+++ test/rails_test.rb
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: 0.0.0 $
+### $Copyright: copyright(c) 2014 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
+libpath = File.class_eval { join(dirname(dirname(__FILE__)), 'lib') }
+$: << libpath unless $:.include?(libpath)
+
+require 'minitest/autorun'
+
+require 'baby_erubis/rails'
+
+
+
+describe 'BabyErubis::RailsTemplate' do
+
+  let(:tmpl) { BabyErubis::RailsTemplate.new }
+
+  def _modify(ruby_code)
+    if (''.freeze).equal?(''.freeze)
+      return ruby_code.gsub(/([^'])';/m, "\\1'.freeze;")
+    else
+      return ruby_code
+    end
+  end
+
+  eruby_template = <<'END'
+<h1>New Article</h1>
+
+<%= form_for :article, url: articles_path do |f| %>
+  <p>
+    <%= f.label :title %><br>
+    <%= f.text_field :title %>
+  </p>
+
+  <p>
+    <%= f.label :text %><br>
+    <%= f.text_area :text %>
+  </p>
+
+  <p>
+    <%= f.submit %>
+  </p>
+<% end %>
+END
+
+  ruby_code = <<'END'
+@output_buffer = output_buffer || ActionView::OutputBuffer.new;@output_buffer.safe_append='<h1>New Article</h1>
+
+';@output_buffer.append= form_for :article, url: articles_path do |f| ;@output_buffer.safe_append='
+  <p>
+    ';@output_buffer.append=(f.label :title);@output_buffer.safe_append='<br>
+    ';@output_buffer.append=(f.text_field :title);@output_buffer.safe_append='
+  </p>
+
+  <p>
+    ';@output_buffer.append=(f.label :text);@output_buffer.safe_append='<br>
+    ';@output_buffer.append=(f.text_area :text);@output_buffer.safe_append='
+  </p>
+
+  <p>
+    ';@output_buffer.append=(f.submit);@output_buffer.safe_append='
+  </p>
+'; end;
+@output_buffer.to_s
+END
+
+
+  describe '#parse()' do
+
+    it "converts eRuby template into ruby code with Rails style." do
+      tmpl = BabyErubis::RailsTemplate.new.from_str(eruby_template)
+      expected = _modify(ruby_code)
+      assert_equal expected, tmpl.src
+    end
+
+  end
+
+
+end
