e7b27727b04db4bf19ddbe276a45583e3c6676cf
makoto kuwata <kwa@kuwata-lab.com>
2015-10-24 08:49:20 +0900

    ruby: add 'lib/baby_erubis/renderer.rb' and define 'BabyErubis::Renderer' module

---
+++ lib/baby_erubis/renderer.rb
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: 0.0.0 $
+### $Copyright: copyright(c) 2014 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
+
+module BabyErubis
+
+  ##
+  ## Module to define template rendering methods.
+  ##
+  ## ex:
+  ##   class MyController
+  ##     include BabyErubis::HtmlEscaper
+  ##     include BabyErubis::Renderer
+  ##
+  ##     #ERUBY_TEMPLATE_DIR       = 'templates'
+  ##     #ERUBY_TEMPLATE_LAYOUT    = :_layout
+  ##     #ERUBY_TEMPLATE_HTML_EXT  = '.html.eruby'
+  ##     #ERUBY_TEMPLATE_TEXT_EXT  = '.eruby'
+  ##     #ERUBY_TEMPLATE_CACHE     = {}
+  ##
+  ##     def index
+  ##       @items = ['A', 'B', 'C']
+  ##       ## renders 'templates/welcome.html.eruby'
+  ##       html = eruby_render_html(:welcome)
+  ##       return html
+  ##     end
+  ##   end
+  ##
+  ##
+  module Renderer
+
+    ERUBY_TEMPLATE_DIR       = '.'
+    ERUBY_TEMPLATE_LAYOUT    = :_layout
+    ERUBY_TEMPLATE_HTML_EXT  = '.html.eruby'
+    ERUBY_TEMPLATE_TEXT_EXT  = '.eruby'
+    ERUBY_TEMPLATE_CACHE     = {}
+
+    def eruby_render_html(template_name, layout: true, encoding: 'utf-8')
+      return _eruby_render_template(template_name, layout) {|tmpl_name|
+        fpath = "#{ERUBY_TEMPLATE_DIR}/#{tmpl_name}#{ERUBY_TEMPLATE_HTML_EXT}"
+        ERUBY_TEMPLATE_CACHE[fpath] ||= BabyErubis::Html.new.from_file(fpath, encoding)
+      }
+    end
+
+    def eruby_render_text(template_name, layout: false, encoding: 'utf-8')
+      return _eruby_render_template(template_name, layout) {|tmpl_name|
+        fpath = "#{ERUBY_TEMPLATE_DIR}/#{tmpl_name}#{ERUBY_TEMPLATE_TEXT_EXT}"
+        ERUBY_TEMPLATE_CACHE[fpath] ||= BabyErubis::Text.new.from_file(fpath, encoding)
+      }
+    end
+
+    private
+
+    def _eruby_render_template(template_name, layout)
+      template = yield template_name
+      s = template.render(self)
+      unless @_layout.nil?
+        layout = @_layout; @_layout = nil
+      end
+      while layout
+        layout = ERUBY_TEMPLATE_LAYOUT if layout == true
+        template = yield layout
+        @_content = s
+        s = template.render(self)
+        @_content = nil
+        layout = @_layout; @_layout = nil
+      end
+      return s
+    end
+
+  end
+
+
+end
---
+++ test/renderer_test.rb
@@ -0,0 +1,187 @@
+# -*- coding: utf-8 -*-
+
+###
+### $Release: 0.0.0 $
+### $Copyright: copyright(c) 2014 kuwata-lab.com all rights reserved $
+### $License: MIT License $
+###
+
+libpath = File.class_eval { join(dirname(dirname(__FILE__)), 'lib') }
+$: << libpath unless $:.include?(libpath)
+
+require 'minitest/autorun'
+
+require 'baby_erubis'
+require 'baby_erubis/renderer'
+
+
+class HelloClass
+  include BabyErubis::HtmlEscaper
+  include BabyErubis::Renderer
+
+  def initialize(vars={})
+    vars.each do |k, v|
+      instance_variable_set("@#{k}", v)
+    end
+  end
+
+end
+
+
+describe 'BabyErubis::Renderer' do
+
+  layout_template = <<'END'
+<!doctype>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title><%= @page_title %></title>
+  </head>
+  <body>
+    <div id="main" class="main">
+<%== @_content %>
+    </div>
+  </body>
+</html>
+END
+
+  html_template = <<'END'
+<%
+   @page_title = 'Example'
+%>
+<h1><%= @page_title %></h1>
+<ul>
+<% for x in @items %>
+  <li><%= x %></li>
+<% end %>
+</ul>
+END
+
+  layout2_template = <<'END'
+<%
+  @_layout = true
+%>
+<section>
+<%== @_content %>
+</section>
+END
+
+  text_template = <<'END'
+title: <%= @title %>
+date:  <%= @date %>
+END
+
+  before do
+    Dir.mkdir('_t')
+    File.open('_t/_layout.html.eruby', 'w') {|f| f.write(layout_template) }
+    File.open('_t/_layout2.html.eruby', 'w') {|f| f.write(layout2_template) }
+    File.open('_t/welcome.html.eruby', 'w') {|f| f.write(html_template) }
+    File.open('_t/example.text.eruby', 'w') {|f| f.write(text_template) }
+  end
+
+  after do
+    Dir.glob('_t/*').each {|fpath| File.unlink(fpath) if File.file?(fpath) }
+    Dir.rmdir('_t')
+  end
+
+
+  describe '#eruby_render_html()' do
+
+    it "renders html template." do
+      obj = HelloClass.new(:items=>[10, 20, 30])
+      actual = obj.eruby_render_html(:'_t/welcome', layout: false)
+      expected = <<'END'
+<h1>Example</h1>
+<ul>
+  <li>10</li>
+  <li>20</li>
+  <li>30</li>
+</ul>
+END
+      assert_equal expected, actual
+    end
+
+    it "renders with layout template." do
+      obj = HelloClass.new(:items=>[10, 20, 30])
+      actual = obj.eruby_render_html(:'_t/welcome', layout: :'_t/_layout')
+      expected = <<'END'
+<!doctype>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Example</title>
+  </head>
+  <body>
+    <div id="main" class="main">
+<h1>Example</h1>
+<ul>
+  <li>10</li>
+  <li>20</li>
+  <li>30</li>
+</ul>
+
+    </div>
+  </body>
+</html>
+END
+      assert_equal expected, actual
+    end
+
+  end
+
+
+  describe '#eruby_render_text()' do
+
+    it "renders text template" do
+      obj = HelloClass.new(:title=>"Homhom", :date=>"2015-01-01")
+      actual = obj.eruby_render_text(:'_t/example.text', layout: false)
+      expected = <<'END'
+title: Homhom
+date:  2015-01-01
+END
+      assert_equal expected, actual
+    end
+
+  end
+
+
+  describe '#_eruby_render_template()' do
+
+    it "recognizes '@_layout' variable" do
+      expected = <<'END'
+<!doctype>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Example</title>
+  </head>
+  <body>
+    <div id="main" class="main">
+<section>
+<h1>Example</h1>
+<ul>
+  <li>10</li>
+  <li>20</li>
+  <li>30</li>
+</ul>
+
+</section>
+
+    </div>
+  </body>
+</html>
+END
+      obj = HelloClass.new(:items=>[10,20,30])
+      File.rename '_t/_layout.html.eruby', '_layout.html.eruby'
+      begin
+        actual = obj.eruby_render_html(:'_t/welcome', layout: :'_t/_layout2')
+        assert_equal expected, actual
+      ensure
+        File.unlink '_layout.html.eruby' if File.exist?('_layout.html.eruby')
+      end
+    end
+
+  end
+
+
+end
